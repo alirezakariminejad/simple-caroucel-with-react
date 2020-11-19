c96bad3a432a118fefb1a04a46350b6ac1cb9a11
Alireza Kariminejad <alireza.kariminejad@yahoo.com>
2020-11-19 20:16:24 +0330

    handlers

--- src/Components/Arrow.jsx
+++ src/Components/Arrow.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Arrow() {
-  return <button type="button" data-role="none" className="slick-prev slick-arrow" aria-label="Previous" role="button"></button>;
+export default function Arrow({ direction, handleClick }) {
+  return <button
+    onClick={handleClick}
+    type="button"
+    data-role="none"
+    className={`slick-${direction} slick-arrow`}
+    aria-label="Previous"
+    role="button">
+    </button>;
 }
